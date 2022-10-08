5813aca54c90670b96a185e3d1c85e710e8753a7
Farid Sakhyzad <farid.sakhyzad@jda.com>
2022-10-09 01:07:55 +0300

    projectile passing process remake

--- src/Components/Projectile/Projectile.jsx
+++ src/Components/Projectile/Projectile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 const PROJECTILE_MOVE_DELAY = 25; //ms per pixel
+const PROJECTILE_MOVE_STEP = 1;
 
 const Projectile = (props) => {
   const {
@@ -24,14 +25,24 @@ const Projectile = (props) => {
   const projectileStateRef = useRef(projectileState);
   projectileStateRef.current = projectileState;
 
+  const ref = useRef(null);
+
   const computedStyle = getComputedStyle(document.documentElement);
 
   const unitRadius = parseInt(computedStyle.getPropertyValue('--unit-hitBox--radius'), 10);
   const projectileWidth = parseInt(computedStyle.getPropertyValue('--projectile-hitBox--width'), 10);
   const projectileHeight = parseInt(computedStyle.getPropertyValue('--projectile-hitBox--height'), 10);
 
-  const launchProjectile = (distance, currentDistance = 0 ) => {
-    if (currentDistance >= distance) {
+  const calculateNewCoords = (coordinateX, coordinateY) => {
+    const theAngle = 90 - angle;
+    const newCoordinateX = PROJECTILE_MOVE_STEP * Math.cos(theAngle * Math.PI / 180);
+    const newCoordinateY = PROJECTILE_MOVE_STEP * Math.sin(theAngle * Math.PI / 180);
+
+    return { coordinateX: coordinateX + newCoordinateX, coordinateY: coordinateY - newCoordinateY }
+  }
+
+  const launchProjectile = (maxDistance, currentDistance = 0, coordinateX = left, coordinateY = top) => {
+    if (currentDistance >= maxDistance) {
       console.log('Stopped by Steps Limit', id);
       return;
     }
@@ -40,30 +51,44 @@ const Projectile = (props) => {
       setProjectileState('inFlight');
     }
 
-    currentDistance += 1;
+    const { coordinateX: currentLeft, coordinateY: currentTop } = calculateNewCoords(coordinateX, coordinateY)
 
-    setTimeout(() => {
+    const timer = setTimeout(() => {
+      // const { top: currentAbsTop, left: currentAbsLeft } = ref.current.getBoundingClientRect();
+      // const currentTop = currentAbsTop - fieldTop;
+      // const currentLeft = currentAbsLeft - fieldLeft;
+
+      console.log('left, top', left, top);
+      console.log('currentLeft, currentTop', currentLeft, currentTop);
+
+      currentDistance = Math.sqrt(Math.pow((currentTop - top), 2) + Math.pow((currentLeft - left), 2));
+
       const outOfField = projectileIsOutOfField(currentDistance);
 
       if (outOfField) {
-        setProjectileState('impact');
-
         onOutOfFiled(id);
 
+        console.log('clearTimeout');
+        clearTimeout(timer);
+
+        setProjectileState('impact');
         return;
       }
 
       const impactedUnit = projectileDidImpact(currentDistance);
-      if (impactedUnit && impactedUnit.value > 0) {
-        setProjectileState('impact');
 
+      if (impactedUnit && impactedUnit.value > 0) {
         onImpact(id, impactedUnit.id);
 
+        console.log('clearTimeout');
+        clearTimeout(timer);
+
+        setProjectileState('impact');
         return;
       }
 
       setProjectileDistance(currentDistance);
-      launchProjectile(distance, currentDistance);
+      launchProjectile(maxDistance, currentDistance, currentLeft, currentTop);
     }, PROJECTILE_MOVE_DELAY);
   };
 
@@ -164,21 +189,30 @@ const Projectile = (props) => {
     });
   }
 
+  const MAX_DISTANCE = 900;
+
   useEffect(() => {
     if (projectileState === 'inFlight' || projectileState === 'impact') {
       return;
     }
 
-    //console.log(`Launch Projectile. ID ${id}. Status ${projectileState}`, '\n\n');
-    launchProjectile(600);
+    console.log(`Launch Projectile. ID ${id}. Status ${projectileState}`, '\n\n');
+    launchProjectile(MAX_DISTANCE);
   }, []);
+
+  console.log('projectileDistance', projectileDistance);
 
   return (
     <div className="projectile"
       id={id}
+      ref={ref}
       style={{
         top,
         left,
+        // '--angle': `${angle}deg`,
+        // '--distance': `${MAX_DISTANCE * -1}px`,
+        // '--duration': `${MAX_DISTANCE * PROJECTILE_MOVE_DELAY}ms`,
+        // animationPlayState: projectileState === 'inFlight' ? 'running' : 'paused'
         transform: `rotate(${angle}deg) translateY(${-1 * projectileDistance}px)`
       }}
       data-state={projectileState}
